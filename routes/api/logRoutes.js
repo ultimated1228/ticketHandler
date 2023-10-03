f8350d10423a4a2ebcaa2980e84bca946fc998d5
n810tran <nathaniel.k.tran@gmail.com>
2023-10-03 14:27:43 -0700

    created connection.js and logRoutes.js

---
+++ routes/api/logRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const { createLog, editLog, deleteLog } = require('../../controllers/logController');
+
+//The routes will match '/api/log/:ticketId?drawer=BOOLEAN' to handle POST calls.
+router.route('/:ticketID')
+
+    .post(createLog)
+//The routes will also match '/api/log/:ticketId/:logId?drawer=BOOLEAN' to handle PUT, and DELETE calls.
+router.route('/:ticketID/:logID')
+    .put(editLog)
+    .delete(deleteLog)
+
+module.exports = router;
