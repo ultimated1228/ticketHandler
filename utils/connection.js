0a28c5a72e66ef177d611a990094200a518bd1f6
galvin-SH <mgalvin1993@gmail.com>
2023-10-04 16:03:34 -0700

    rewrote seed script seed data and connection, fixed typo in ticketRoutes

--- utils/connection.js
+++ utils/connection.js
@@ -1,21 +1,14 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PW,
-        {
-            host: process.env.host,
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
+    host: process.env.HOST,
+    dialect: 'mysql',
+    dialectOptions: {
+        decimalNumbers: true,
+    },
+});
 
 module.exports = sequelize;
