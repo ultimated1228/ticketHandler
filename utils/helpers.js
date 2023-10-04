493a5e835da2b27764946001da49964ee6fdbdc7
galvin-SH <mgalvin1993@gmail.com>
2023-10-04 13:28:15 -0700

    added findDiff helper to compare deep equivalence of the two supplied objects

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,7 +3,23 @@ module.exports = {
         return date.toLocaleTimeString();
     },
     format_date: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear() + 5
-            }`;
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
+    findDiff: (newValues, { oldValues }) => {
+        const newKeys = Object.keys(newValues)
+        const oldKeys = Object.keys(oldValues)
+        if (newKeys.length != oldKeys.length) return 0;
+        let output = [];
+        for (var key of newKeys) {
+            const valueFrom = newValues[key];    // Compare from newvalue
+            const valueAgainst = oldValues[key]; // Against oldvalue
+            if (valueFrom != valueAgainst) {
+                output.push(
+                    `${key} was changed from ${oldValues[key]} to ${newValues[key]}`
+                )
+            }
+        }
+        if (output.length == 0) return 0;
+        return output;
+    }
 };
