0fcbf3e8ca9b7f2ffa2c4aa8e119565f661efe7c
moe-j22 <kmoe9262@gmail.com>
2023-10-05 10:26:18 -0700

    saving work

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
-const { loginUser, logoutUser} = require('../../controllers/userController');
-
+const { loginUser, logoutUser} = require('../../controllers/userController')
 router.route('/')
     .post(loginUser)
     .delete(logoutUser)
