f33ef1f923366311f5bba0751dd44fef5f2bdf9d
galvin-SH <mgalvin1993@gmail.com>
2023-10-05 12:10:35 -0700

    home routes built, home controller WIP

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
 
-// router.use('/',homeRoutes);
+// routes for controlling views
+router.use('/',homeRoutes);
+
+// routes for controlling data
 router.use('/api',apiRoutes);
 
 module.exports = router;
