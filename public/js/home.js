90f10783aa63bb3712d93b631c73cf9f2054eb9e
moe-j22 <kmoe9262@gmail.com>
2023-10-07 09:56:18 -0700

    saving work

---
+++ public/js/home.js
@@ -0,0 +1,70 @@
+const { Ticket, User } = require ('../../models');
+
+const serializeData = (tickets) => {
+    return tickets.map((ticket) => ({
+      id: ticket.id,
+      subject: ticket.subject,
+      description: ticket.description,
+      status: ticket.status,
+      urgency: ticket.urgency,
+      client: {
+        id: ticket.client.id,
+        firstName: ticket.client.firstName,
+        lastName: ticket.client.lastName,
+      },
+      tech: (ticket.tech) ? {
+        id: ticket.tech.id,
+        firstName: ticket.tech.firstName,
+        lastName: ticket.tech.lastName,
+      } : null,
+    }));
+  };
+
+const getDashboardData = async (req, res) => {
+    try {
+      const { status } = req.params;
+      const { userId, role } = req.session;
+  
+      let tickets;
+  
+      if (role === 'client') {
+        tickets = await Ticket.findAll({
+          where: {
+            clientId: userId,
+            isArchived: false,
+          },
+          include: [{ model: User, as: 'client' }],
+        });
+      } else if (role === 'tech') {
+        const techOptions = {
+          where: {
+            techId: userId,
+            isArchived: false,
+          },
+          include: [{ model: User, as: 'client' }, { model: User, as: 'tech' }],
+        };
+  
+        if (status) {
+          techOptions.where.status = status;
+        }
+  
+        tickets = await Ticket.findAll(techOptions);
+      }
+  
+      const serializedData = serializeData(tickets); 
+      
+      res.render('dashboard', {
+        layout: 'main',
+        title: 'Dashboard',
+        loggedIn: true,
+        userType: role,
+        data: serializedData,
+      });
+    } catch (error) {
+      console.error('Error in dashboard controller:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
+  
+  module.exports = { getDashboardData };
